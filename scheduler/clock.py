8450de6e4ef3d9a4d883414bd44146a9e76dae85
ohad <ohadnav@gmail.com>
2022-12-23 11:59:47 +0200

    fix scheduling variable

--- scheduler/clock.py
+++ scheduler/clock.py
@@ -4,7 +4,7 @@ from image_changes import image_changes_task
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', hour=1)
+@scheduler.scheduled_job('interval', hours=1)
 def hourly_image_changes_task():
     image_changes_task.product_images_task()
 
