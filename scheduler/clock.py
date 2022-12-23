bc47777585574b3ac9108b18896afc645432cd8d
ohad <ohadnav@gmail.com>
2022-12-23 12:11:05 +0200

    add logging

--- scheduler/clock.py
+++ scheduler/clock.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from image_changes import image_changes_task
@@ -6,6 +8,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', hours=1)
 def hourly_image_changes_task():
+    print('Running hourly image changes task time is {}'.format(datetime.now()))
     image_changes_task.product_images_task()
 
 scheduler.start()
