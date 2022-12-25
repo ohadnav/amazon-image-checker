fd534834ea6853bf24b91b1572c359148a73e754
ohad <ohadnav@gmail.com>
2022-12-25 12:27:49 +0200

    add logging

--- scheduler/clock.py
+++ scheduler/clock.py
@@ -10,11 +10,12 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', hours=1)
 def hourly_image_changes_task():
-    util.set_default_logging_format()
-    util.set_default_logging_level_to_debug()
     logging.debug('Running hourly image changes task time is {}'.format(datetime.now()))
     image_changes_task.product_images_task()
 
 
 if __name__ == '__main__':
+    util.set_default_logging_format()
+    util.set_default_logging_level_to_debug()
+    logging.info('Starting scheduler')
     scheduler.start()
