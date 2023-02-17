e41f431493b13249d54fa413e844343c86be5c7e
ohad <ohadnav@gmail.com>
2023-02-17 23:14:19 +0200

    Revert "adjusted clock interval"
    This reverts commit fcd9461f

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-clock: python scheduler/clock.py
+clock: python scheduler/ab_test_clock.py
+clock: python scheduler/image_changes_clock.py
--- scheduler/clock.py
+++ scheduler/clock.py
@@ -4,23 +4,17 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from common import util
-from scheduler.ab_test_task import ABTestTask
 from scheduler.image_changes_task import ImageChangesTask
 
 scheduler = BlockingScheduler()
 
 
 @scheduler.scheduled_job('interval', hours=int(os.environ['SCHEDULER_INTERVAL_HOURS']))
-def hourly_image_changes_task():
+def image_changes_task_scheduler():
     ImageChangesTask().run()
-
-
-@scheduler.scheduled_job('interval', hours=int(os.environ['SCHEDULER_INTERVAL_HOURS']))
-def hourly_ab_test_task():
-    ABTestTask().run()
 
 
 if __name__ == '__main__':
     util.initialize_debug_logging()
-    logging.info('Starting scheduler')
+    logging.info(f'Starting scheduler {os.path.basename(__file__)}')
     scheduler.start()
